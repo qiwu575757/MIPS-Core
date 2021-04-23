ba7d64e7e7e7438b7f8607cc012161e699215a52
王翰墨 <8188164+wang-hanmo@user.noreply.gitee.com>
2021-04-23 16:42:08 +0800

    解决了M级例外同时E级store指令时出现的访存问题

--- hazard.v
+++ hazard.v
@@ -1,4 +1,4 @@
-module bypath(ID_EX_RS, ID_EX_RT, IF_ID_RS, IF_ID_RT, EX_MEM_RD, MEM_WB_RD, EX_MEM_RFWr, MEM_WB_RFWr, 
+module bypass(ID_EX_RS, ID_EX_RT, IF_ID_RS, IF_ID_RT, EX_MEM_RD, MEM_WB_RD, EX_MEM_RFWr, MEM_WB_RFWr, 
 			EX_MEM_RHLWr, ID_EX_RHLSelRd, EX_MEM_RHLSelWr, MEM_WB_RHLWr, MEM_WB_RHLSelWr, BJOp, 
 			MUX4Sel, MUX5Sel, MUX8Sel, MUX9Sel, MUX10Sel);
 	input EX_MEM_RFWr, MEM_WB_RFWr, BJOp, EX_MEM_RHLWr, MEM_WB_RHLWr, ID_EX_RHLSelRd;
@@ -55,20 +55,26 @@ module bypath(ID_EX_RS, ID_EX_RT, IF_ID_RS, IF_ID_RT, EX_MEM_RD, MEM_WB_RD, EX_M
 endmodule
 
 module stall(ID_EX_RT, EX_MEM_RT, IF_ID_RS, IF_ID_RT, ID_EX_DMRd, ID_PC, EX_PC, EX_MEM_DMRd, PCWr, IF_IDWr, MUX7Sel, BJOp, ID_EX_RFWr,
-				ID_EX_CP0Rd, EX_MEM_CP0Rd, rst_sign, inst_sram_en);
+				ID_EX_CP0Rd, EX_MEM_CP0Rd, rst_sign, inst_sram_en, EX_MEM_ex);
 	input[4:0] ID_EX_RT, EX_MEM_RT, IF_ID_RS, IF_ID_RT;
 	input [31:0] ID_PC, EX_PC;
 	input ID_EX_DMRd, EX_MEM_DMRd, BJOp, ID_EX_RFWr;
-	input ID_EX_CP0Rd, EX_MEM_CP0Rd;
+	input ID_EX_CP0Rd, EX_MEM_CP0Rd, EX_MEM_ex;
 	input rst_sign;
 	output reg PCWr, IF_IDWr, MUX7Sel, inst_sram_en;
 
-	always@(ID_EX_RT, IF_ID_RS, IF_ID_RT, ID_EX_DMRd, EX_MEM_RT,EX_MEM_DMRd, BJOp, ID_EX_RFWr, rst_sign)
+	always@(ID_EX_RT, IF_ID_RS, IF_ID_RT, ID_EX_DMRd, EX_MEM_RT,EX_MEM_DMRd, BJOp, ID_EX_RFWr, rst_sign, EX_MEM_ex)
 	    if(rst_sign) begin
 			inst_sram_en = 1'b0;
 			PCWr = 1'b0;
 			IF_IDWr = 1'b0;
 			MUX7Sel = 1'b1;
+		end
+		else if(EX_MEM_ex) begin
+			inst_sram_en = 1'b1;
+			PCWr = 1'b1;
+			IF_IDWr = 1'b1;
+			MUX7Sel = 1'b0;
 		end
 		else if((ID_EX_DMRd || ID_EX_CP0Rd) && ( (ID_EX_RT == IF_ID_RS) || (ID_EX_RT == IF_ID_RT) ) &&(ID_PC != EX_PC)) begin
 		    inst_sram_en = 1'b0;
