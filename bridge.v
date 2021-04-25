eafd415a370e471bfae4dee131481b3234712b07
王翰墨 <8188164+wang-hanmo@user.noreply.gitee.com>
2021-04-25 20:51:03 +0800

    将RHL移到bridge_RHL中的temp

--- bridge.v
+++ bridge.v
@@ -43,7 +43,7 @@ module bridge_dm(
 												4'b1111 ;//sw
 
 
-	assign data_sram_addr={addr1[31:2],2'b0}-32'ha000_0000;
+	assign data_sram_addr= {addr1[31:2],2'b0}-32'ha000_0000;
 	assign dout=
 				DMSel2==3'b011 ?  // zero
 				(  	addr2[1:0]==2'b00 ? {24'b0,data_sram_rdata[7:0]} :
@@ -104,15 +104,14 @@ module bridge_dm(
 
 endmodule
 
-
 // 这个模块用于当前与cpu与乘除器的交互。
 // 借助状态机来控制
 // * start为1时，乘除法开始计算
 // * isBusy为1时，表示正在运行
 // * 乘法单周期运算，除法多周期（34个）。
 // * C是运算结果，支持都保存，即如果没有新的start，结果会保持为上一次的运算结果
-module brodge_RHL(
-		aclk,aresetn,A, B, C, ALU2Op,start,isBusy,
+module bridge_RHL(
+		aclk,aresetn, A, B, ALU2Op,start, isBusy, RHLOut, EX_RHLWr, EX_RHLSel_Wr, EX_RHLSel_Rd, MEM_Exception, MEM_eret_flush
 	);
 input aclk;
 input aresetn;
@@ -120,8 +119,13 @@ input [31:0 ]A;
 input [31:0] B;
 input [1:0] ALU2Op;
 input start;
+input EX_RHLWr;
+input[1:0] EX_RHLSel_Wr;
+input EX_RHLSel_Rd;
+input MEM_Exception;
+input MEM_eret_flush;
 output isBusy;
-output [63:0] C;
+output[31:0] RHLOut;
 wire [63:0] divider_sign_out;
 wire [63:0] divider_unsign_out;
 wire [63:0] multi_sign_out;
@@ -131,15 +135,15 @@ reg present_state;
 reg next_state;
 wire m_axis_dout_tvalid1;
 wire m_axis_dout_tvalid2;
-
 wire[63:0] tempA, tempB;
+
+assign RHLOut = EX_RHLSel_Rd ? temp[63:32] : temp[31:0];
 
 assign tempA = A[31] ? {32'hffffffff,A} : {32'h00000000,A};
 assign tempB = B[31] ? {32'hffffffff,B} : {32'h00000000,B};
 
 
-assign C=temp;
-assign isBusy=present_state;
+assign isBusy=next_state;
 
 parameter state_free = 1'b0 ;
 parameter state_busy = 1'b1 ;
@@ -148,23 +152,29 @@ parameter state_busy = 1'b1 ;
     // 2'b01: C = tempA * tempB;
     // 2'b10: C = {R1,Q1};//unsigned
     // 2'b11: C = {R2,Q2};//signed
-assign multi_sign_out=A*B;
-assign multi_unsign_out=tempA*tempB;
+assign multi_unsign_out=A*B;
+assign multi_sign_out=tempA*tempB;
 
 
 always @(posedge aclk) begin
-	if(ALU2Op==2'b00 &&start)
-		temp<=multi_sign_out;
-	else if(ALU2Op==2'b01 &&start)
-		temp<=multi_unsign_out;
+    if(!aresetn)
+        temp <= 64'd0;
+	else if(ALU2Op==2'b00 && start && !MEM_Exception && !MEM_eret_flush)
+		temp <= multi_unsign_out;
+	else if(ALU2Op==2'b01 && start && !MEM_Exception && !MEM_eret_flush)
+		temp <= multi_sign_out;
 	else if(m_axis_dout_tvalid1) //sign
-		temp<=divider_sign_out;
+		temp <= {divider_sign_out[31:0],divider_sign_out[63:32]};
 	else if(m_axis_dout_tvalid2 ) //unsign
-		temp<=divider_unsign_out;
+		temp <= {divider_unsign_out[31:0],divider_unsign_out[63:32]};
+	else if(EX_RHLWr && EX_RHLSel_Wr == 2'b01)
+	    temp <= {A,temp[31:0]};
+	else if(EX_RHLWr && EX_RHLSel_Wr == 2'b00)
+	    temp <= {temp[63:32],A};   
 end
 
 always @(posedge aclk ) begin
-	if(aresetn)
+	if(!aresetn)
 	begin
 		present_state=state_free;
 	end
@@ -174,15 +184,20 @@ always @(posedge aclk ) begin
 	end
 end
 
-always @(posedge aclk ) begin
-	if(start&ALU2Op[1])
-	begin
-		next_state=state_busy;
+always @(present_state, start, ALU2Op, m_axis_dout_tvalid1, m_axis_dout_tvalid2) begin
+	if(present_state == state_free) begin
+	   if(start && ALU2Op[1] && !MEM_Exception && !MEM_eret_flush)
+	       next_state=state_busy;
+	   else
+	       next_state=state_free;
 	end
-	else if (m_axis_dout_tvalid1|m_axis_dout_tvalid2)
-	begin
-		next_state=state_free;
+	else begin
+	   if (m_axis_dout_tvalid1|m_axis_dout_tvalid2)
+		   next_state=state_free;
+	   else
+	       next_state=state_busy;
 	end
+
 end
 
 
@@ -190,9 +205,9 @@ end
 Divider divider (
   .aclk(aclk),                                      // input wire aclk
   .aresetn(aresetn),                                // input wire aresetn
-  .s_axis_divisor_tvalid(start&ALU2Op[1]&ALU2Op[0]),    // input wire s_axis_divisor_tvalid
+  .s_axis_divisor_tvalid(start && ALU2Op[1] && ALU2Op[0] && !MEM_Exception && !MEM_eret_flush),    // input wire s_axis_divisor_tvalid
   .s_axis_divisor_tdata(B),      // input wire [31 : 0] s_axis_divisor_tdata
-  .s_axis_dividend_tvalid(start&ALU2Op[1]&ALU2Op[0]),  // input wire s_axis_dividend_tvalid
+  .s_axis_dividend_tvalid(start && ALU2Op[1] && ALU2Op[0] && !MEM_Exception && !MEM_eret_flush),  // input wire s_axis_dividend_tvalid
   .s_axis_dividend_tdata(A),    // input wire [31 : 0] s_axis_dividend_tdata
   .m_axis_dout_tvalid(m_axis_dout_tvalid1),          // output wire m_axis_dout_tvalid
   .m_axis_dout_tdata(divider_sign_out)            // output wire [63 : 0] m_axis_dout_tdata
@@ -200,9 +215,9 @@ Divider divider (
 Divider_Unsighed divider_unsign (
   .aclk(aclk),                                      // input wire aclk
   .aresetn(aresetn),                                // input wire aresetn
-  .s_axis_divisor_tvalid(start&ALU2Op[1]&~ALU2Op[0]),    // input wire s_axis_divisor_tvalid
+  .s_axis_divisor_tvalid(start && ALU2Op[1] && !ALU2Op[0] && !MEM_Exception && !MEM_eret_flush),    // input wire s_axis_divisor_tvalid
   .s_axis_divisor_tdata(B),      // input wire [31 : 0] s_axis_divisor_tdata
-  .s_axis_dividend_tvalid(start&ALU2Op[1]&~ALU2Op[0]),  // input wire s_axis_dividend_tvalid
+  .s_axis_dividend_tvalid(start && ALU2Op[1] && !ALU2Op[0] && !MEM_Exception && !MEM_eret_flush),  // input wire s_axis_dividend_tvalid
   .s_axis_dividend_tdata(A),    // input wire [31 : 0] s_axis_dividend_tdata
   .m_axis_dout_tvalid(m_axis_dout_tvalid2),          // output wire m_axis_dout_tvalid
   .m_axis_dout_tdata(divider_unsign_out)            // output wire [63 : 0] m_axis_dout_tdata
