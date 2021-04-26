1bbff234ec43d3b691646a487d36993f47a6aa81
黑客奇兵 <7556831+wuqi5757@user.noreply.gitee.com>
2021-04-27 00:40:16 +0800

    更改了部分信号的命名，模块的顺序，添加了部分注释

--- cp0.v
+++ cp0.v
@@ -9,33 +9,38 @@
 
 `define status_bev          Status[22]
 `define status_im           Status[15:8]
-`define status_exl          Status[1]      //鍙戠敓渚嬪鏃惰浣嶈缃负1
-`define status_ie           Status[0]      //鍏ㄥ眬涓柇浣胯兘浣?
-`define cause_bd            Cause[31]      //鏈?杩戝彂鐢熶緥澶栫殑鎸囦护鏄惁澶勪簬鍒嗘敮寤惰繜妲?
-`define cause_ti            Cause[30]      //璁℃椂鍣ㄤ腑鏂寚绀?
-`define cause_ip1           Cause[15:10]    //寰呭鐞嗙‖浠讹紙IP7..IP2锛変腑鏂爣璇?
-`define cause_ip2           Cause[9:8]      //寰呭鐞嗚蒋浠讹紙IP1..IP0锛変腑鏂爣璇?
-`define cause_excode        Cause[6:2]     //渚嬪缂栫爜
+`define status_exl          Status[1]      //例外级
+`define status_ie           Status[0]      //全局中断使能位
+`define cause_bd            Cause[31]      //是否处于分支延迟槽
+`define cause_ti            Cause[30]      //计时器中断提示
+`define cause_ip1           Cause[15:10]    //待处理硬件中断，每一位对应一个中断线
+`define cause_ip2           Cause[9:8]      //待处理软件中断标识
+`define cause_excode        Cause[6:2]     //例外编码
 
 
-module CP0(clk, rst, CP0WrEn, addr, data_in, EX_MEM_Exc, EX_MEM_eret_flush, EX_MEM_bd,
-            ext_int_in, EX_MEM_ExcCode, EX_MEM_badvaddr, EX_MEM_PC, data_out, EPC_out, Interrupt);
+module CP0(
+    clk, rst, CP0WrEn, addr, data_in, MEM_Exc, 
+    MEM_eret_flush, MEM_bd,ext_int_in, 
+    MEM_ExcCode, MEM_badvaddr, MEM_PC,
+
+    data_out, EPC_out, Interrupt
+);
     input clk;
     input rst;
     input CP0WrEn;
     input [7:0] addr;
     input [31:0] data_in;
-    input EX_MEM_Exc;           //璁垮瓨闃舵鐨勪緥澶栦俊鍙?
-    input EX_MEM_eret_flush;           //eret鎸囦护淇敼EXL鍩熺殑浣胯兘淇″彿浠ュ強杩斿洖鍜屽埛鏂版祦姘寸嚎淇″彿
-    input EX_MEM_bd;            //璁垮瓨闃舵鐨勬寚浠ゆ槸鍚︽槸寤惰繜妲芥寚浠?
-    input [5:0] ext_int_in;       //澶勭悊鍣ㄦ牳椤跺眰鐨?6涓腑鏂緭鍏ヤ俊鍙?
-    input [4:0] EX_MEM_ExcCode;        //璁垮瓨闃舵鐨勪緥澶栫紪鐮?
-    input [31:0] EX_MEM_badvaddr;      //璁垮瓨闃舵鐨勫嚭閿欒櫄鍦板潃
-    input [31:0] EX_MEM_PC;             //璁垮瓨闃舵鐨凱C
+    input MEM_Exc;           //M级中断标识
+    input MEM_eret_flush;           //M级eret指令清空信号
+    input MEM_bd;            //延迟槽标识
+    input [5:0] ext_int_in;       //外部硬件中断标识
+    input [4:0] MEM_ExcCode;        //M级例外编码
+    input [31:0] MEM_badvaddr;      //出错虚拟地址
+    input [31:0] MEM_PC;             
 
     output [31:0] data_out;
     output [31:0] EPC_out;
-    output Interrupt;           //涓柇淇″彿
+    output Interrupt;           //中断
 
     reg [31:0] BadVAddr;
     reg [31:0] Count;
@@ -43,8 +48,8 @@ module CP0(clk, rst, CP0WrEn, addr, data_in, EX_MEM_Exc, EX_MEM_eret_flush, EX_M
     reg [31:0] Status;
     reg [31:0] Cause;
     reg [31:0] EPC;
-    wire count_eq_compare;       //Count瀵勫瓨鍣ㄥ拰Compare瀵勫瓨鍣ㄧ浉绛変俊鍙?
-    reg tick;                   //鏃堕挓棰戠巼鐨勪竴鍗?
+    wire count_eq_compare;       //Count == Compare
+    reg tick;                   
     reg epc_sign;               //epc_sign = 0,例外改写epc；epc_sign = 1,指令改写epc；
 
     assign count_eq_compare = (Compare == Count);
@@ -52,7 +57,7 @@ module CP0(clk, rst, CP0WrEn, addr, data_in, EX_MEM_Exc, EX_MEM_eret_flush, EX_M
     assign Interrupt = 
         ((Cause[15:8] & `status_im) != 8'h00) && `status_ie == 1'b1 && `status_exl == 1'b0;
 
-    //MFC0鎸囦护璇诲彇CP0
+    //MFC0读取CP0
     assign data_out = 
                 (addr == `BadVAddr_index )    ? BadVAddr :
                 (addr == `Count_index)        ? Count:
@@ -61,13 +66,13 @@ module CP0(clk, rst, CP0WrEn, addr, data_in, EX_MEM_Exc, EX_MEM_eret_flush, EX_M
                 (addr == `Cause_index )       ? Cause :
                 (addr == `EPC_index )         ? EPC + epc_sign * 4:
                                     0;
-    //BadVAddr瀵勫瓨鍣?
+    //BadVAddr generation
     always @(posedge clk) begin
-        if (EX_MEM_Exc && (EX_MEM_ExcCode == `AdEL || EX_MEM_ExcCode == `AdES))
-            BadVAddr <= EX_MEM_badvaddr;
+        if (MEM_Exc && (MEM_ExcCode == `AdEL || MEM_ExcCode == `AdES))
+            BadVAddr <= MEM_badvaddr;
     end
 
-    //Count瀵勫瓨鍣?
+    //Count generation
     always @(posedge clk) begin
         if (!rst)
             tick <= 1'b0;
@@ -80,37 +85,37 @@ module CP0(clk, rst, CP0WrEn, addr, data_in, EX_MEM_Exc, EX_MEM_eret_flush, EX_M
             Count <= Count + 1'b1;
     end
 
-    //Compare瀵勫瓨鍣?
+    //Compare generation
     always @(posedge clk) begin
         if (CP0WrEn && addr == `Compare_index)
             Compare <= data_in;
     end
 
-    //Status瀵勫瓨鍣ㄧ殑bev鍩?
+    //Status
     always @(posedge clk) begin
         if (!rst)
             `status_bev <= 1'b1;
     end
 
-    //Status瀵勫瓨鍣ㄧ殑IM7~IM0鍩?
+    //Status
     always @(posedge clk) begin
         if (CP0WrEn && addr == `Status_index)
             `status_im <= data_in[15:8];
     end
 
-    //Status瀵勫瓨鍣ㄧ殑EXL鍩?
+    //Status
     always @(posedge clk) begin
         if (!rst)
             `status_exl <= 1'b0;
-        else if (EX_MEM_Exc)
+        else if (MEM_Exc)
             `status_exl <= 1'b1;
-        else if (EX_MEM_eret_flush)
+        else if (MEM_eret_flush)
             `status_exl <= 1'b0;
         else if (CP0WrEn && addr == `Status_index)
             `status_exl <= data_in[1];
     end
 
-    //Status瀵勫瓨鍣ㄧ殑IE鍩?
+    //Status
     always @(posedge clk) begin
         if (!rst)
             `status_ie <= 1'b0;
@@ -118,7 +123,7 @@ module CP0(clk, rst, CP0WrEn, addr, data_in, EX_MEM_Exc, EX_MEM_eret_flush, EX_M
             `status_ie <= data_in[0];
     end
 
-    //Status瀵勫瓨鍣ㄧ殑闆跺煙
+    //Status
     always @(posedge clk) begin
         if (!rst) begin
             Status[31:23] <= 9'b0;
@@ -127,15 +132,15 @@ module CP0(clk, rst, CP0WrEn, addr, data_in, EX_MEM_Exc, EX_MEM_eret_flush, EX_M
         end
     end
 
-    //Cause瀵勫瓨鍣ㄧ殑BD鍩?
+    //Cause
     always @(posedge clk) begin
         if (!rst)
             `cause_bd <= 1'b0;
-        else if (EX_MEM_Exc && !`status_exl)
-            `cause_bd <= EX_MEM_bd;
+        else if (MEM_Exc && !`status_exl)
+            `cause_bd <= MEM_bd;
     end
 
-    //Cause瀵勫瓨鍣ㄧ殑TI鍩?
+    //Cause
     always @(posedge clk) begin
         if (!rst)
             `cause_ti <= 1'b0;
@@ -145,7 +150,7 @@ module CP0(clk, rst, CP0WrEn, addr, data_in, EX_MEM_Exc, EX_MEM_eret_flush, EX_M
             `cause_ti <= 1'b1;
     end
 
-    //Cause瀵勫瓨鍣ㄧ殑IP7~IP2鍩?
+    //Cause
     always @(posedge clk) begin
         if (!rst)
             `cause_ip1 <= 6'b0;
@@ -155,7 +160,7 @@ module CP0(clk, rst, CP0WrEn, addr, data_in, EX_MEM_Exc, EX_MEM_eret_flush, EX_M
         end
     end
 
-    //Cause瀵勫瓨鍣ㄧ殑IP1鍜孖P0鍩?
+    //Cause
     always @(posedge clk) begin
         if (!rst)
             `cause_ip2 <= 2'b0;
@@ -163,15 +168,15 @@ module CP0(clk, rst, CP0WrEn, addr, data_in, EX_MEM_Exc, EX_MEM_eret_flush, EX_M
             `cause_ip2 <= data_in[9:8];
     end
 
-    //Cause瀵勫瓨鍣ㄧ殑Excode鍩?
+    //Cause
     always @(posedge clk) begin
         if (!rst)
             `cause_excode <= 5'b0;
-        else if (EX_MEM_Exc)
-            `cause_excode <= EX_MEM_ExcCode;
+        else if (MEM_Exc)
+            `cause_excode <= MEM_ExcCode;
     end
 
-    //Cause瀵勫瓨鍣ㄧ殑闆跺煙
+    //Cause
     always @(posedge clk) begin
         if (!rst) begin
             Cause[29:16] <= 14'b0;
@@ -180,12 +185,12 @@ module CP0(clk, rst, CP0WrEn, addr, data_in, EX_MEM_Exc, EX_MEM_eret_flush, EX_M
         end
     end
 
-    //EPC瀵勫瓨鍣?
+    //EPC
     always @(posedge clk) begin
         if (!rst)
             epc_sign <= 0;
-        if (EX_MEM_Exc && !`status_exl) begin
-            EPC <= EX_MEM_bd ? EX_MEM_PC - 4 : EX_MEM_PC  ;
+        if (MEM_Exc && !`status_exl) begin
+            EPC <= MEM_bd ? MEM_PC - 4 : MEM_PC;
             epc_sign <= 0;
         end   
         else if (CP0WrEn && addr == `EPC_index) begin
