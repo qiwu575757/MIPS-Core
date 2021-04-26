469659118cd29a7f7f1f41add5465c6916106a10
黑客奇兵 <7556831+wuqi5757@user.noreply.gitee.com>
2021-04-27 00:45:30 +0800

    更改了接口信号的顺序

--- rf.v
+++ rf.v
@@ -1,4 +1,8 @@
-module rf(Addr1, Addr2, Addr3, WD, RFWr, clk, rst, RD1, RD2);
+module rf(
+	Addr1, Addr2, Addr3, WD, RFWr, clk, rst, 
+	
+	RD1, RD2
+);
 	input[4:0] Addr1, Addr2, Addr3;
 	input[31:0] WD;
 	input RFWr, clk, rst;
